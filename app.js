83beb76b7e1f1a1f452d40b947e79191c994712a
Krzysztof Lojniewski <kontakt@pagepro.pl>
2017-04-06 23:06:50 +0200

    is dev condition

--- app.js
+++ app.js
@@ -45,6 +45,9 @@ class App {
   }
   createBTCBuyOrders () {
     Logger.info('Bot Init, getting account informations.')
+    if (Env.IS_DEV) {
+      return
+    }
     Promise.all([
       this.Bitbay.getPLNBalance(),
       Order.findActive()
@@ -111,10 +114,18 @@ class App {
 
         if (isBought) {
           Logger.success(`#${dbOrderId} has been bought! Changing order status.`)
-          databaseOrder.saveUpdatedStatus(Env.STATUS_BOUGHT)
-        } else if (isActive || isInActive) {
+          if (!Env.IS_DEV) {
+            databaseOrder.saveUpdatedStatus(Env.STATUS_BOUGHT)
+          }
+        } else if (isActive) {
           const priceMargin = Number(databaseOrder.buyPrice - buyPrice).toFixed(2)
           Logger.info(`#${dbOrderId} is waiting, ${databaseOrder.buyPrice} vs ${buyPrice} (${priceMargin} PLN)`)
+        } else if (isInActive) {
+          Logger.info(`#${dbOrderId} found as INACTIVE.`)
+          if (!Env.IS_DEV) {
+            databaseOrder.apiResponseBuy = this.Checker.getInactiveOrder(inActiveOrders, dbOrderId)
+            databaseOrder.saveUpdatedStatus(Env.STATUS_INACTIVE_BUY)
+          }
         } else {
           lostOrders.push(databaseOrder)
         }
@@ -150,10 +161,18 @@ class App {
         if (isSold) {
           const profit = Number(dbOrder.estimatedProfit).toFixed(2)
           Logger.success(`#${dbOrderId} has been sold! Profit: ${profit} PLN. Changing Order status.`)
-          dbOrder.saveUpdatedStatus(Env.STATUS_SOLD)
-        } else if (isActive || isInActive) {
+          if (!Env.IS_DEV) {
+            dbOrder.saveUpdatedStatus(Env.STATUS_SOLD)
+          }
+        } else if (isActive) {
           const priceMargin = Number(dbOrder.sellPrice - sellPrice).toFixed(2)
           Logger.sell(`#${dbOrderId} is waiting, ${dbOrder.sellPrice} vs ${sellPrice} (${priceMargin} PLN)`)
+        } else if (isInActive) {
+          if (!Env.IS_DEV) {
+            Logger.info(`#${dbOrderId} found as INACTIVE.`)
+            dbOrder.apiResponseSell = this.Checker.getInactiveOrder(inActiveOrders, dbOrderId)
+            dbOrder.saveUpdatedStatus(Env.STATUS_INACTIVE_SELL)
+          }
         } else {
           lostOrders.push(dbOrder)
         }
@@ -165,6 +184,9 @@ class App {
     })
   }
   createBTCSellOrders () {
+    if (Env.IS_DEV) {
+      return
+    }
     Order.findByStatusId(Env.STATUS_BOUGHT).then(orders => {
       if (orders.length) {
         Logger.info(`Creating ${orders.length} BTC Sell Order(s)`)
